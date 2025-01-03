ffee199e42996a7df925254b00b0f1ad9dc10bfd
Pushkar Bhatt <pushkarbhatt231@gmail.com>
2025-01-03 16:59:51 +0530

    Create zomato_data_analysis.sql

---
+++ zomato_data_analysis.sql
@@ -0,0 +1,26 @@
+The SQL code provided is designed to manage and analyze data related to Zomato users, their signups, sales transactions, and products. Here's a breakdown of the key components.
+  
+Queries:
+
+1. Calculate the total amount each customer spent on Zomato.
+
+2. Count the number of distinct days each customer visited Zomato.
+
+3. Identifies the first product purchased by each customer.
+
+4. Determine the items most purchased on the menu and the number of times all customers purchased them.
+
+5. Find the most popular item for each customer.
+
+6. Identifies the first item purchased by the customer after becoming a gold member.
+
+7. Identifies the item purchased before the customer became a member.
+
+8. Calculates the total orders and amount each member spends before becoming a member.
+
+9. Calculates Zomato points earned by customers in the first year after joining the gold program.
+
+10. Ranks all transactions of the customers.
+
+This SQL code provides a comprehensive way to manage and analyze Zomato's user and sales data, helping to gain insights into customer behavior and preferences.
+If you have any specific questions or need further details, feel free to ask!
